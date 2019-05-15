7c34913574748beaf81140ae63a04fee5399814a
root <root@lad01.oblivion.online>
2019-05-15 06:27:07 +0000

    V1.1 Bootstrap Generator - Corrected bootstap asset naming

--- gen_bootstrap.sh
+++ gen_bootstrap.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 TAG=$(date +"%m.%d.%y")
+DATE=$(date)
 TITLE='Bootstrap'
-DESC='Blockchain imported into bootstrap.dat as of $(date).'
+DESC='Blockchain imported into bootstrap.dat as of'
+SCRIPTROOT='/root/.mnscripts/'
 FILEROOT='/root/.lemonad/blocks/'
 FILENAME='bootstrap.dat'
 
 # make new bootstrap.dat
-cd $FILEROOT
-ls -1 blk[0-9]*.dat | sort | xargs cat > $FILENAME
+ls -1 $FILEROOT/blk[0-9]*.dat | sort | xargs cat > $FILENAME
 
 # make your tag and upload
 git tag $TAG && git push --tags
-gothub upload --user switchlove --repo masternode-install --tag $TAG --name "$Title" --description "$DESC" --file $FILEROOT/$FILENAME
-
+gothub release --user switchlove --repo masternode-install --tag $TAG --name "$TITLE" --description "$DESC $DATE."
+gothub upload --user switchlove --repo masternode-install --tag $TAG --name "$FILENAME" --file $FILENAME
